316363c08c30fef0da6389119fa5beae11d0edd4
Julien Pivotto <roidelapluie@inuits.eu>
2012-02-13 20:10:02 +0100

    Module openswan: test connection Host internet2:

---
+++ manifests/connection.pp
@@ -0,0 +1,16 @@
+define openswan::connection (
+  $left,
+  $leftsubnet,
+  $right,
+  $rightsubnet,
+  $esp,
+  $ike,
+
+) {
+    file {
+      "/etc/ipsec.d/${name}.conf":
+        ensure  => present,
+        content => template('openswan/connection.erb'),
+
+    }
+}
