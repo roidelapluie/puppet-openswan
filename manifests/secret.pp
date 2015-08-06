9c03c183241ebcc148a8dcb623c79bf62189fb79
Julien Pivotto <roidelapluie@inuits.eu>
2015-08-06 23:38:46 +0200

    Fix linting

--- manifests/secret.pp
+++ manifests/secret.pp
@@ -1,10 +1,10 @@
 define openswan::secret (
-  $local_ip = '%any',
   $remote_ip,
-  $secret
+  $secret,
+  $local_ip = '%any',
 ) {
   file {
-    "/etc/ipsec.d/$name.secrets":
+    "/etc/ipsec.d/${name}.secrets":
       ensure  => present,
       content => "${local_ip} ${remote_ip}: PSK \"${secret}\"\n",
       require => Package['openswan'],
